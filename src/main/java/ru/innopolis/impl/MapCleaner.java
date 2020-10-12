3ac3d419d85e16ce82cc083f8b8374e57f9e7eb4
YuraVolkov <yuramilkyway@gmail.com>
2020-10-12 16:25:11 +0300

    Добавление документации MapCleaner

--- src/main/java/ru/innopolis/impl/MapCleaner.java
+++ src/main/java/ru/innopolis/impl/MapCleaner.java
@@ -4,6 +4,17 @@ import java.util.Map;
 import java.util.Set;
 
 public class MapCleaner {
+    /**
+     * Метод принимающий карту(map) и две коллекции строк.
+     * В карте, посредством reflection поля, перечисленные
+     * в fieldsToClenup устанавливает в значение null.
+     * @param map Карта в которой удаляем ключи.
+     * @param fieldsToCleanup Поля, перечисленные в fieldsToClenup
+     * устанавливаем в значение null.
+     * @param fieldsToOutput Поля, перечисленные в fieldsToOutput
+     * конвертируем в строку (вызвав toString у объектов, или String.valueOf для примитивных типов)
+     * и выводим результат преобразования в консоль
+     */
     public void cleanup(Map map, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) {
         validateArguments(map, fieldsToCleanup);
         validateArguments(map, fieldsToOutput);
@@ -17,6 +28,12 @@ public class MapCleaner {
         }
     }
 
+    /**
+     * При отсутствии в мапе нужных ключей - вызываем
+     * IllegalArgumentException, оставив объект неизменным.
+     * @param map Мапа, в который ищем ключи.
+     * @param names Множество ключей.
+     */
     private void validateArguments(Map map, Set<String> names) {
         for (String name : names) {
             if (!(map.containsKey(name))) {
@@ -25,10 +42,20 @@ public class MapCleaner {
         }
     }
 
+    /**
+     * Заменяем ключи в мапе.
+     * @param fieldName Ключ.
+     * @param map Карта в которой чистим ключи.
+     */
     private void cleanField(String fieldName, Map map) {
         map.replace(fieldName, null);
     }
 
+    /**
+     * Печатаем ноды карты по указанному ключу.
+     * @param fieldName Ключ.
+     * @param map Карта.
+     */
     private void printField(String fieldName, Map map) {
         if (map.get(fieldName) != null) {
             System.out.println(map.get(fieldName).toString());
